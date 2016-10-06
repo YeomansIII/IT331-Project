b78fe4f1cfbff46c9b684abef0e09f443877c5c5
Jason Yeomans <jyeo098@gmail.com>
2016-10-06 16:00:38 -0400

    create all pages, set up grunt

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        clean: ['dist'],
+        includes: {
+            html: {
+                files: [{
+                    src: '**/*.html',
+                    dest: 'dist',
+                    flatten: false,
+                    cwd: 'src',
+                    options: {
+                        silent: true
+                    }
+                }]
+            },
+            css: {
+                files: [{
+                    src: '**/*.css',
+                    dest: 'dist',
+                    flatten: false,
+                    cwd: 'src',
+                    options: {
+                        silent: true
+                    }
+                }]
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        watch: {
+            html: {
+                files: ['src/**/*.html'],
+                tasks: ['includes:html']
+            },
+            css: {
+                files: ['src/**/*.css'],
+                tasks: ['includes:css']
+            },
+            images: {
+                files: ['src/**/*.{png,jpg,gif}'],
+                tasks: ['imagemin']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-includes');
+
+    grunt.registerTask('default', [
+        'clean',
+        'imagemin',
+        'includes'
+    ]);
+    grunt.registerTask('serve', [
+        'clean',
+        'imagemin',
+        'includes',
+        'watch'
+    ]);
+
+};
